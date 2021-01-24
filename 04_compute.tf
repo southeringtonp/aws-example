cd68ad22081e075ec4efa451103c31cf1cfdf106
Paul Southerington <nospam@southerington.com>
2021-01-24 13:09:54 -0500

    File renames

--- 04_compute.tf
+++ 04_compute.tf
@@ -1,15 +1,16 @@
 ############################################################
-# Private Web Server - No Inbound Internet
+# Bastion Host - Publicly accessible
 ############################################################
 
-resource "aws_network_interface" "nic3" {
-    subnet_id = aws_subnet.sub3.id
-    private_ips = ["10.0.3.10"]
-    security_groups = [ aws_security_group.sg_web.id ]
+resource "aws_network_interface" "nic1" {
+    subnet_id = aws_subnet.sub1.id
+    private_ips = ["10.0.1.10"]
+    security_groups = [ aws_security_group.sg_ssh.id ]
 }
 
-resource "aws_instance" "compute3" {
+resource "aws_instance" "compute1" {
     ami = data.aws_ami.rhel8.id         # ami-096fda3c22c1c990a / RHEL 8.3
+
     instance_type = "t2.micro"
     instance_initiated_shutdown_behavior = "terminate"
 
@@ -19,16 +20,23 @@ resource "aws_instance" "compute3" {
         delete_on_termination = "true"
         volume_size = 20
     }
-    user_data = file("./webserver-init.sh")
+    user_data = file("./yum-update.sh")
 
     depends_on = [aws_internet_gateway.igw]
     network_interface {
-        network_interface_id = aws_network_interface.nic3.id
+        network_interface_id = aws_network_interface.nic1.id
         device_index = 0
     }
 
     tags = {
-        Name = "RHEL 8 Private"
+        Name = "RHEL 8 Public"
     }
 }
+
+resource "aws_eip" "eip1" {
+    vpc = true
+    instance = aws_instance.compute1.id
+    depends_on = [aws_internet_gateway.igw]
+}
+
 
--- 05_compute.tf
+++ 05_compute.tf
@@ -1,16 +1,15 @@
 ############################################################
-# Bastion Host - Publicly accessible
+# Private Web Server - No Inbound Internet
 ############################################################
 
-resource "aws_network_interface" "nic1" {
-    subnet_id = aws_subnet.sub1.id
-    private_ips = ["10.0.1.10"]
-    security_groups = [ aws_security_group.sg_ssh.id ]
+resource "aws_network_interface" "nic3" {
+    subnet_id = aws_subnet.sub3.id
+    private_ips = ["10.0.3.10"]
+    security_groups = [ aws_security_group.sg_web.id ]
 }
 
-resource "aws_instance" "compute1" {
+resource "aws_instance" "compute3" {
     ami = data.aws_ami.rhel8.id         # ami-096fda3c22c1c990a / RHEL 8.3
-
     instance_type = "t2.micro"
     instance_initiated_shutdown_behavior = "terminate"
 
@@ -20,23 +19,16 @@ resource "aws_instance" "compute1" {
         delete_on_termination = "true"
         volume_size = 20
     }
-    user_data = file("./yum-update.sh")
+    user_data = file("./webserver-init.sh")
 
     depends_on = [aws_internet_gateway.igw]
     network_interface {
-        network_interface_id = aws_network_interface.nic1.id
+        network_interface_id = aws_network_interface.nic3.id
         device_index = 0
     }
 
     tags = {
-        Name = "RHEL 8 Public"
+        Name = "RHEL 8 Private"
     }
 }
-
-resource "aws_eip" "eip1" {
-    vpc = true
-    instance = aws_instance.compute1.id
-    depends_on = [aws_internet_gateway.igw]
-}
-
 
